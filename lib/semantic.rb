eddffd914b1de18d02ba668044a48eeb1c1ee071
Fabio Yamate <fabioyamate@gmail.com>
2009-12-03 01:39:46 -0200

    Changed Semantic implementation to support scopes and reduce scope tape and concat scopes when closed

--- lib/semantic.rb
+++ lib/semantic.rb
@@ -2,8 +2,10 @@ require File.dirname(__FILE__) + '/lazycomb_compiler'
 
 class Semantic
   REDUCTIONS_TERMS_LENGTH = { :i => 2, :I => 2, :s => 4, :k => 3 }
+  @@scopes = [[]]
   
-  def self.reduce(list)
+  def self.reduce
+    list = self.scope
     return list unless REDUCTIONS_TERMS_LENGTH[list.first].eql? list.size
     
     case list.first
@@ -16,4 +18,22 @@ class Semantic
     end
   end
   
+  def self.scope
+    @@scopes.last
+  end
+  
+  def self.new_scope
+    @@scopes << []
+  end
+  
+  def self.close_scope
+    raise 'Error: cannot close global scope' if @@scopes.size <= 1
+    self.reduce
+    reduced = @@scopes.pop
+    self.scope.concat(reduced)
+  end
+  
+  def self.reset_scope
+    @@scopes = [[]]
+  end
 end
--- spec/semantic_spec.rb
+++ spec/semantic_spec.rb
@@ -2,23 +2,56 @@ require File.dirname(__FILE__) + '/spec_helper'
 require 'semantic' 
 
 describe Semantic do
+  before(:each) do
+    Semantic.reset_scope
+  end
+  
   it "should not reduce an empty tape" do
-    reduction = Semantic.reduce([])
+    reduction = Semantic.reduce
     reduction.should be_empty
   end
   
   it "should reduce SIII to II(II)" do
-    reduction = Semantic.reduce([:s, :I, :I, :I])
+    Semantic.scope.concat([:s, :I, :I, :I])
+    reduction = Semantic.reduce
     reduction.should == [:I, :I, :lpar, :I, :I, :rpar]
   end
   
   it "should reduce II to I" do
-    reduction = Semantic.reduce([:I, :I])
+    Semantic.scope.concat([:I, :I])
+    reduction = Semantic.reduce
     reduction.should == [:I]
   end
   
   it "should reduce KKI to K" do
-    reduction = Semantic.reduce([:k, :k, :I])
+    Semantic.scope.concat([:k, :k, :I])
+    reduction = Semantic.reduce
     reduction.should == [:k]
+  end
+  
+  it "should be able to differenciate inputs with scopes" do
+    Semantic.scope << :s
+    Semantic.scope.should include(:s)
+    Semantic.new_scope
+    Semantic.scope << :i
+    Semantic.scope.should include(:i)
+    Semantic.scope.should_not include(:s)
+  end
+  
+  it "should concat reduced combinators when scopes are closed" do
+    Semantic.scope << :s
+    Semantic.scope.should include(:s)
+    Semantic.new_scope
+    Semantic.scope << :i
+    Semantic.scope.should include(:i)
+    Semantic.scope.should_not include(:s)
+    Semantic.close_scope
+    Semantic.scope.should include(:s, :i)
+  end
+  
+  it "should not allow close global scope" do
+    lambda {
+      Semantic.close_scope
+    }.should raise_error
   end
 end
